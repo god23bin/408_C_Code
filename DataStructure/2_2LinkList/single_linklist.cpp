fd97df1e66a081c80f197fb42773009d9067b864
god23bin <479017580@qq.com>
2021-05-11 23:17:10 +0800

    第一题-删除单链表相同x

--- DataStructure/2_2LinkList/single_linklist.cpp
+++ DataStructure/2_2LinkList/single_linklist.cpp
@@ -320,6 +320,22 @@ void test() {
 	PrintList(L);
 }
 
+// 1.解答
+void Del_X_3(LinkList& L, ElemType x) {
+	// 递归实现在单链表L中删除值为x的结点
+	LNode* p;					// p指向待删除结点
+	if (L == NULL) return;		// 递归出口
+	if (L->data == x) {			// 若L所指结点的值为x
+		p = L;					// 删除* L， 并让L指向下一结点
+		L = L->next;
+		free(p);
+		Del_X_3(L, x);			// 递归调用
+	}
+	else {						// 若L所指结点的值不为x
+		Del_X_3(L->next, x);	// 递归调用
+	}
+}
+
 int main() {
 	test();
 	return 0;
