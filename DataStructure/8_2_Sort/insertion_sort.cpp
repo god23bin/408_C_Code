f7d0eaa09b82e53a260aa5b65caf18c60e5dfb61
god23bin <479017580@qq.com>
2021-07-14 22:13:38 +0800

    直接插入排序的实现，同时练习了几次代码的编写

--- DataStructure/8_2_Sort/insertion_sort.cpp
+++ DataStructure/8_2_Sort/insertion_sort.cpp
@@ -68,3 +68,141 @@ void InsertionSort3(int A[], int n) {
 }
 
 // 对链表插入排序 - 还是O(n^2)
+
+// 直接插入排序练习
+void InsertionSortPractice(int arr[], int n) {
+	int i, j, temp;
+	for (i = 1; i < n; i++) {
+		if (arr[i] < arr[i - 1]) {
+			temp = arr[i];
+			for (j = i - 1; j >= 0 && arr[j] > temp; j--) {
+				arr[j + 1] = arr[j];
+			}
+			arr[j + 1] = temp;
+		}
+	}
+}
+
+void IS1(int arr[], int n) {
+	int i, j, temp;
+	for (i = 1; i < n; i++) {
+		if (arr[i] < arr[i - 1]) {
+			temp = arr[i];
+			for (j = i - 1; j >= 0 && arr[j] > temp; j--) {
+				arr[j + 1] = arr[j];
+			}
+			arr[j + 1] = temp;
+		}
+	}
+}
+
+
+
+//  23 24  6  9  3 35
+//   6 23 24
+// 直接插入排序的代码实现
+
+void IS2(int arr[], int n) {
+	int i, j, temp;
+	for (i = 1; i < n; i++) {
+		if (arr[i] < arr[i - 1]) {
+			temp = arr[i];
+			for (j = i - 1; j >= 0 && arr[j] > temp; j--) {
+				arr[j + 1] = arr[j];
+			}
+			arr[j + 1] = temp;
+		}
+	}
+}
+
+
+void IS3(int A[], int n) {
+	int i, j, temp;
+	for (i = 1; i < n; i++) {
+		if (A[i] < A[i - 1]) {
+			temp = A[i];
+			for (j = i - 1; j >= 0 && A[j] > temp; j--) {
+				A[j + 1] = A[j];
+			}
+			A[j + 1] = temp;
+		}
+	}
+}
+
+
+// 带哨兵
+void IS4(int A[], int n) {
+	int i, j;
+	for (i = 2; i < n; i++) {
+		if (A[i] < A[i - 1]) {
+			A[0] = A[i];
+			for (j = i - 1; j >= 0 && A[j] > A[0]; j--) {
+				A[j + 1] = A[j];
+			}
+			A[j + 1] = A[0];
+		}
+	}
+}
+
+
+// 折半插入排序
+void IS5(int arr[], int n) {
+	int i, j, temp;
+	for (i = 1; i < n; i++) {
+		if (arr[i] < arr[i - 1]) {
+			temp = arr[i];
+			int low = 0, high = i - 1, mid;
+			while (low <= high) {
+				mid = (low + high) / 2;
+				if (arr[mid] > temp) {
+					high = mid - 1;
+				}
+				else {
+					low = mid + 1;
+				}
+			}
+			for (j = i - 1; j >= high + 1; j--) {
+				arr[j + 1] = arr[j];
+			}
+			arr[j + 1] = temp;
+		}
+	}
+}
+
+
+// 插入排序
+void IS6(int arr[], int n) {
+	int i, j, temp;
+	for (i = 1; i < n; i++) {
+		if (arr[i] < arr[i - 1]) {
+			temp = arr[i];
+			for(j = i - 1; j >=0 && arr[j] > temp; j--) {
+				arr[j + 1] = arr[j];
+			}
+			arr[j + 1] = temp;
+		}
+	}
+}
+
+
+// 折半插入排序
+int IS7(int arr[], int n) {
+	int i, j, temp;
+	for (int i = 1; i < n; i++) {
+		if (arr[i] < arr[i - 1]) {
+			temp = arr[i];
+			int low = 0, high = i - 1, mid;
+			while (low < high) {
+				mid = (low + high) / 2;
+				if (arr[mid] > temp) 
+					high = mid - 1;
+				else 
+					low = mid + 1;
+			}
+			for (j = i - 1; j >= high + 1; j--) {
+				arr[j + 1] = arr[j];
+			}
+			arr[j + 1] = temp;
+		}
+	}
+}
